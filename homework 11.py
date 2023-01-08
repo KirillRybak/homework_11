92a032a94b8a69337c735bd0deeaf8fac86660f0
KirillRybak <kiryz.rybak.00@mail.ru>
2023-01-08 19:38:07 +0300

    TypeError

--- homework 11.py
+++ homework 11.py
@@ -1,10 +1,14 @@
-a = int(input("Введите число, которое делим  "))
-b = "hello world"
+
 try:
+    a = int(input("Введите число, которое делим  "))
+    b = 105
     c = a + b
 except TypeError:
     print("Упс, что то пошло не так")
+except ValueError:
+    print("И снова что то пошло не так")
 finally:
     print("Всё работает")
+
 
 
