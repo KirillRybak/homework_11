b5c4d214f2c9d2978e0a467d70a6cb9cd1d78deb
KirillRybak <kiryz.rybak.00@mail.ru>
2023-01-10 09:56:35 +0300

    TypeError

--- homework11.py
+++ homework11.py
@@ -1,11 +1,17 @@
-day = int(input("Введите день вашего рождения "))
-month = int(input("Введите месяц рождения "))
-years = int(input("Введите год рождения "))
-day_now = int(input("Введите текущий день "))
-month_now = int(input("Введите текущий месяц "))
-years_now = int(input("Введите текущий год "))
-s_years = years_now - years                  #число лет, без расчёта месяцев
-s_month = month + month_now
-if s_month >= 12:
-    s_years = s_years - 1
-print(s_years)
+try:
+    years = int(input("Введите год рождения"))
+    years_now = int(input("Введите текущий год"))
+    age_years = years_now - years
+    month = int(input("Введите месяц рождения"))
+    month_now = int(input("Введите текущий месяц"))
+    age_month = month_now - month
+    day = int(input("Введите день рождения"))
+    day_now = int(input("Введите текущий день"))
+    age_day = day_now - day
+    if month > month_now:
+        age_years = age_years - 1
+    elif day > day_now:
+        age_years = age_years - 1
+    print(age_years)
+except ValueError:
+    print("Упс, что то пошло не так")
